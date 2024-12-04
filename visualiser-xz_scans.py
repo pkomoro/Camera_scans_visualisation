8a698fb99008de2edf905da99cf5e4f3b80d3b96
PK <p.komorowski@imio.pw.edu.pl>
2024-12-04 14:46:00 +0100

    Added automated z-range reading

--- visualiser-xz_scans.py
+++ visualiser-xz_scans.py
@@ -6,16 +6,23 @@ import pathlib
 if __name__ == "__main__":
 
     # path to the folder containing .npy files
-    path ="C:/Users/komor/OneDrive - Wojskowa Akademia Techniczna/Pomiary/NN-wideband_lens"
+    path ="C:/Users/komor/OneDrive - Wojskowa Akademia Techniczna/Pomiary/Ring_lens"
 
     
     paths = [f for f in Path(path).glob("*.npy")]
 
+    paths_meta = [f for f in Path(path).glob("*.meta")]
     
     data = paths.copy()
 
+    data_meta = paths_meta.copy()
+
     for i in range(len(paths)):
         data[i] = np.load(Path(paths[i]))
+
+        file = open(Path(paths_meta[i]), "r")
+        data_meta[i] = file.read()
+        file.close()
 
     for i in range(len(paths)):
         paths[i] = paths[i].absolute().as_posix()[:-4]
@@ -23,13 +30,33 @@ if __name__ == "__main__":
         
 
     for i in range(len(paths)):
-        image = np.swapaxes(data[i], 0, 2)
+        index = data_meta[i].find("Step Z")
+        data_meta[i] = data_meta[i][(index+8):]
 
-        y = 15
+        index = data_meta[i].find("mm")
+        z_step = float(data_meta[i][:(index-1)])
+
+        index = data_meta[i].find("Start Z")
+        data_meta[i] = data_meta[i][(index+9):]
+
+        index = data_meta[i].find("mm")
+        z_start = float(data_meta[i][:(index-1)])
+
+        index = data_meta[i].find("Stop Z")
+        data_meta[i] = data_meta[i][(index+8):]
+
+        index = data_meta[i].find("mm")
+        z_stop = float(data_meta[i][:(index-1)])
+
+
+        image = np.swapaxes(data[i], 0, 2)
+        image = np.flip(image,2)
+
+        y = 48
 
         plt.xlabel('z [mm]')
         plt.ylabel('x [mm]')
-        plt.imshow(image[y,:,:], cmap='inferno')
+        plt.imshow(image[y,:,:], cmap='inferno', aspect = 'auto',extent=[301 - z_stop, 301 - z_start, -72, 72])
         plt.savefig(paths[i] + '_xz_y' + str(y) + 'px.jpg', dpi = 1000)
         plt.savefig(paths[i] + '_xz_y' + str(y) + 'px.svg')
 
